452a7c68e4da1d724fa1624c2e5493405b57579c
Tanguy BERASATEGUY <tanguy.legeek@gmail.com>
2022-07-25 15:29:02 +0200

    Implementation of Composite design pattern and documentation

--- connectedLibrary/src/main/java/ghostfire059/java/connectedLibrary/Group.java
+++ connectedLibrary/src/main/java/ghostfire059/java/connectedLibrary/Group.java
@@ -1,17 +1,32 @@
 package ghostfire059.java.connectedLibrary;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ * Composite design pattern
+ * Uses to store multiple LibraryEntity and is also considered as one
+ * @author tberasateguy
+ *
+ */
 public class Group implements LibraryEntity
 {
 	private Collection<LibraryEntity> _list = new ArrayList<LibraryEntity>();
 	private String _title;
 	
+	/**
+	 * To create a Group, you only need a title
+	 * @param title
+	 */
 	public Group(String title)
 	{
 		this._title = title;
@@ -129,5 +144,86 @@ public class Group implements LibraryEntity
 			}
 		});
 		return newObject;
+	}
+
+	@Override
+	public Iterator<String> getEditors()
+	{
+		Collection<String> tmpEditor = new HashSet<String>();
+		this._list.forEach(libraryEntity -> 
+		{
+			Iterator<String> childEditor = libraryEntity.getEditors();
+			while(childEditor.hasNext())
+			{
+				tmpEditor.add(childEditor.next());
+			}
+		});
+		return tmpEditor.iterator();
+	}
+
+	@Override
+	public boolean setEditor(String editor)
+	{
+		this._list.forEach(libraryEntity -> libraryEntity.setEditor(editor));
+		return true;
+	}
+
+	@Override
+	public Iterator<String> getType()
+	{
+		Collection<String> tmpType = new HashSet<String>();
+		this._list.forEach(libraryEntity ->
+		{
+			Iterator<String> childType = libraryEntity.getType();
+			while(childType.hasNext())
+			{
+				tmpType.add(childType.next());
+			}
+		});
+		return tmpType.iterator();
+	}
+
+	@Override
+	public boolean setType(String type)
+	{
+		this._list.forEach(libraryEntity -> libraryEntity.setType(type));
+		return true;
+	}
+	
+	/**
+	 * add a LibraryEntity to the current Group as a child
+	 * @param libraryEntity
+	 * @return true if done correctly, false else
+	 */
+	public boolean addChild(LibraryEntity libraryEntity)
+	{
+		return this._list.add(libraryEntity);
+	}
+	
+	/**
+	 * remove a LibraryEntity to the current Group
+	 * @param libraryEntity
+	 * @return true if done correctly, false else
+	 */
+	public boolean removeChild(LibraryEntity libraryEntity)
+	{
+		return this._list.remove(libraryEntity);
+	}
+
+	@Override
+	public boolean export(String filename) throws IOException
+	{
+		String newFilename = filename + this._title + "/";
+		Path path = Paths.get(newFilename);
+		if(!Files.exists(path))
+		{			
+			Files.createDirectory(path);
+		}
+		
+		for(LibraryEntity libraryEntity : this._list)
+		{
+			libraryEntity.export(newFilename);
+		}
+		return true;
 	}
 }
