0e7fca4df3c1741a82f70b68cb6632be311b24c9
Tanguy BERASATEGUY <tanguy.legeek@gmail.com>
2022-07-25 12:24:12 +0200

    flobal minor project modifications

--- connectedLibrary/src/main/java/module-info.java
+++ connectedLibrary/src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module ghostfire059.java.connectedLibrary {
     requires javafx.controls;
 	requires htmlunit;
+	requires jdk.scripting.nashorn;
+	requires json.simple;
     exports ghostfire059.java.connectedLibrary;
 }
